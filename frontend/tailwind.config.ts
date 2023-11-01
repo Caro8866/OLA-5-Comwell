fbfbde25adc0c01e1b50d8aa2193d1a456a8075a
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-01 11:41:27 +0100

    chore: add colors to tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,48 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        earth: {
+          100: "#677169",
+          80: "#808B82",
+          60: "#A0ABA3",
+          40: "#C0C8C2",
+          20: "#E3E6E3",
+          10: "#EEEFEE",
+        },
+        sand: {
+          100: "#AA8D65",
+          80: "#BEA179",
+          60: "#D7C9B9",
+          40: "#EDE3D8",
+          20: "#F0EAE2",
+          10: "#F9F6F2",
+        },
+        sea: {
+          100: "#182B39",
+          80: "#273A48",
+          60: "#9696A1",
+          40: "#C5C5CB",
+          20: "#E1E1E5",
+          10: "#F3F3F5",
+        },
+        charcoal: {
+          100: "#161D20",
+          80: "#252C2F",
+          60: "#9F9898",
+          40: "#D3D1D1",
+          20: "#EAE9E7",
+          10: "#F6F4F3",
+        },
+        errorRed: "#EB0026",
+        successGreen: "#2ECC71",
+        warningYellow: "#F1C40F",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
