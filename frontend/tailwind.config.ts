eaf4b0bffa4fba6cd4c0b8514dc52f92740196aa
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-01 13:24:37 +0100

    chore: add screen sizes to tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,6 +4,13 @@ const config: Config = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
       colors: {
         earth: {
           100: "#677169",
