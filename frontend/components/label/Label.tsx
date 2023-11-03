0aa13070bee482d70ad625bf1cfe9c78a0a3fe4d
Fryderyk Boncler <f.relcnob@gmail.com>
2023-11-03 12:09:40 +0100

    fix: resolved problem with color variants

--- frontend/components/label/Label.tsx
+++ frontend/components/label/Label.tsx
@@ -6,13 +6,40 @@ type Props = {
 };
 
 function Label({ color, children }: Props) {
-  return (
-    <span
-      className={`py-1 px-3 bg-${color}-100 rounded-full text-xs font-medium font-sans`}
-    >
-      {children}
-    </span>
-  );
+  switch (color) {
+    case "charcoal":
+      return (
+        <span
+          className={`py-1 px-3 bg-charcoal-80 rounded-full text-xs font-medium font-sans text-slate-50`}
+        >
+          {children}
+        </span>
+      );
+    case "sea":
+      return (
+        <span
+          className={`py-1 px-3 bg-sea-80 rounded-full text-xs font-medium font-sans text-slate-50`}
+        >
+          {children}
+        </span>
+      );
+    case "sand":
+      return (
+        <span
+          className={`py-1 px-3 bg-sand-80 rounded-full text-xs font-medium font-sans text-slate-50`}
+        >
+          {children}
+        </span>
+      );
+    case "earth":
+      return (
+        <span
+          className={`py-1 px-3 bg-earth-80 rounded-full text-xs font-medium font-sans text-slate-50`}
+        >
+          {children}
+        </span>
+      );
+  }
 }
 
 export default Label;
--- frontend/pages/test/index.tsx
+++ frontend/pages/test/index.tsx
@@ -1,3 +1,4 @@
+import "@/app/globals.css";
 import Label from "@/components/label/Label";
 
 function test() {
