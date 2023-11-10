d4119d6eb04ce92f841d70e668ca528f6ecc32a8
George <georgenico9@gmail.com>
2023-11-10 01:32:26 +0100

    chore: set up default sturcture and CRUD operations for users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/comwell')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/comwell'), UsersModule],
   controllers: [],
   providers: [],
 })
