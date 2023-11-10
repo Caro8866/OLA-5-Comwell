c7aeaa3f87cac593b8c3c9383114405745f70205
George <georgenico9@gmail.com>
2023-11-10 04:30:58 +0100

    feat: add full user validation on sign up

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,10 +1,20 @@
-import { IsNotEmpty, IsEmail, Validate } from 'class-validator';
+import { IsNotEmpty, IsEmail, Validate, IsInt, IsIn } from 'class-validator';
 import { CustomPasswordValidator } from './validators/CustomPasswordValidator';
+import { IsDateOfBirthValid } from './validators/isDateOfBirthValid';
 
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Please enter a name.' })
+  fullName: string;
+
   @IsNotEmpty({ message: 'Email cannot be empty.' })
   @IsEmail()
   email: string;
+
+  @IsInt()
+  zipCode: number;
+
+  @IsInt()
+  phone: number;
 
   @IsNotEmpty({ message: 'Password cannot be empty.' })
   @Validate(CustomPasswordValidator, {
@@ -12,4 +22,17 @@ export class CreateUserDto {
       'Password must be at least 6 characters long and contain at least one uppercase letter and one number.',
   })
   password: string;
+
+  @IsNotEmpty({ message: 'Gender cannot be empty.' })
+  @IsIn(['Prefer not to say', 'Male', 'Female', 'Other'], {
+    message:
+      'Invalid gender. Allowed values are "Prefer not to say", "Male", "Female" or ""Other.',
+  })
+  gender: string;
+
+  // @IsDate({ message: 'Invalid date of birth.' })
+  @Validate(IsDateOfBirthValid, {
+    message: 'The person must be at least 18 years old.',
+  })
+  dateOfBirth: Date;
 }
--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -3,13 +3,35 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
+enum Gender {
+  Male = 'Male',
+  Female = 'Female',
+  'Prefer not to say' = 'Prefer not to say',
+  Other = 'Other',
+}
+
 @Schema()
 export class User {
+  @Prop()
+  fullName: string;
+
   @Prop({ unique: true })
   email: string;
 
   @Prop()
+  zipCode: number;
+
+  @Prop()
+  phone: number;
+
+  @Prop()
   password: string;
+
+  @Prop({ type: String, enum: Object.values(Gender) })
+  gender: Gender;
+
+  @Prop()
+  dateOfBirth: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
