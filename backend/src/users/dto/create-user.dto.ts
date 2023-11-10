d1839c2de87e7b691ff51d364d09ca77579e0eef
George <georgenico9@gmail.com>
2023-11-10 03:30:48 +0100

    feat: add validation to username and pass with dto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,11 +1,15 @@
-import { IsNotEmpty, IsEmail, MinLength, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsEmail, Validate } from 'class-validator';
+import { CustomPasswordValidator } from './validators/CustomPasswordValidator';
+
 export class CreateUserDto {
   @IsNotEmpty({ message: 'Email cannot be empty.' })
   @IsEmail()
   email: string;
 
-  @IsNotEmpty({ message: 'Password cannot be Empty.' })
-  @MinLength(6, { message: 'Password must be 6 characters.' })
-  @MaxLength(128, { message: 'Password must be less than 128.' })
+  @IsNotEmpty({ message: 'Password cannot be empty.' })
+  @Validate(CustomPasswordValidator, {
+    message:
+      'Password must be at least 6 characters long and contain at least one uppercase letter and one number.',
+  })
   password: string;
 }
