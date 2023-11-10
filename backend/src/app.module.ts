d36d30a8647d750763bb64811f885d615f94338d
George <georgenico9@gmail.com>
2023-11-10 05:22:37 +0100

    chore: add base for sign in

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/comwell'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/comwell'), UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
