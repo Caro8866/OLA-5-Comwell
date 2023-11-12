1c8ff89ceaa050ca02aa326b6597c2b3070c13e4
George <georgenico9@gmail.com>
2023-11-12 22:25:12 +0100

    fix: typo on file name

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constans';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
