9f802b9762dd1bf1693d2bfb2e6be63b58ce9322
George <georgenico9@gmail.com>
2023-11-13 10:26:56 +0100

    feat: implent env variable for jwt secret

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,15 +3,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
+import 'dotenv/config';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '60s' },
     }),
   ],
