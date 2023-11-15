571b253f88de97b83f6aa72e0d29301908b4cb95
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-15 20:10:30 +0100

    fix: mongodb connection string

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/comwell'), UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongodb:27017/comwell'),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
