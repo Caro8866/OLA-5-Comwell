5ce7b2be741d0e09d25b0b89d00aececdcc2707c
George <georgenico9@gmail.com>
2023-11-16 16:26:51 +0100

    fix: connext to local host

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongodb:27017/comwell'),
+    MongooseModule.forRoot('mongodb://localhost:27017/comwell'),
     UsersModule,
     AuthModule,
   ],
