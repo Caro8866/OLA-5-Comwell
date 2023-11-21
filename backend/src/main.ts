1318f10f1b889aac81de8ffffa942d88ea978534
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-21 18:44:26 +0100

    fix: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
