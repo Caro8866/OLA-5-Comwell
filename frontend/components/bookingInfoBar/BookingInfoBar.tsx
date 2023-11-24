97cb475f09b9da76c268ca0bd00b41bfc1cb15e2
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-24 15:53:30 +0100

    feat: booking info bar

--- frontend/components/bookingInfoBar/BookingInfoBar.tsx
+++ frontend/components/bookingInfoBar/BookingInfoBar.tsx
@@ -9,29 +9,35 @@ type BookingInfoBarProps = {
   totalPrice: number;
 };
 
-function BookingInfoBar({ checkInDate, checkOutDate, totalRooms, totalPersons, hotelLocation, totalPrice }: BookingInfoBarProps) {
+const BookingInfoBar: React.FC<BookingInfoBarProps> = ({ checkInDate, checkOutDate, totalRooms, totalPersons, hotelLocation, totalPrice }) => {
   return (
-    <div className="flex items-center justify-between px-4 py-2 bg-gray-100">
-      <div className="flex items-center">
+    <div className=" flex items-center h-[68px] text-xs bg-white">
+      <span className="border-r border-gray-300 px-4 flex items-center gap-2">
         {/* Icon for dates */}
-        <span className="mx-2">{`${checkInDate} - ${checkOutDate}`}</span>
-      </div>
-
-      <div className="flex items-center">
+        <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          {/* SVG Path */}
+        </svg>
+        {`${checkInDate} - ${checkOutDate}`}
+      </span>
+      <span className="border-r border-gray-300 px-4 flex items-center gap-2">
         {/* Icon for rooms and persons */}
-        <span className="mx-2">{`${totalRooms} room${totalRooms > 1 ? "s" : ""}, ${totalPersons} person${totalPersons > 1 ? "s" : ""}`}</span>
-      </div>
-
-      <div className="flex items-center">
+        <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          {/* SVG Path */}
+        </svg>
+        {`${totalRooms} room${totalRooms > 1 ? "s" : ""}, ${totalPersons} person${totalPersons > 1 ? "s" : ""}`}
+      </span>
+      <span className="border-r border-gray-300 px-4 flex items-center gap-2">
         {/* Icon for location */}
-        <span className="mx-2">{hotelLocation}</span>
-      </div>
-
-      <div className="flex items-center">
-        <span className="mx-2">{totalPrice} kr.</span>
+        <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          {/* SVG Path */}
+        </svg>
+        {hotelLocation}
+      </span>
+      <div className="flex items-center pl-4">
+        <span>{totalPrice} kr.</span>
       </div>
     </div>
   );
-}
+};
 
 export default BookingInfoBar;
