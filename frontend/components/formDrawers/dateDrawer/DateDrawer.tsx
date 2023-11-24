35d559e9e38083c58b22437ab92986ba3741a4dc
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-24 15:54:05 +0100

    fix: edit dateDrawer props and date handling

--- frontend/components/formDrawers/dateDrawer/DateDrawer.tsx
+++ frontend/components/formDrawers/dateDrawer/DateDrawer.tsx
@@ -1,19 +1,29 @@
 import Button from "@/components/button/Button";
 import Heading from "@/components/text/heading/Heading";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
 
 type DateDrawerProps = {
   isOpen: boolean;
   onClose: () => void;
-  startDate: Date | null;
+  startDate: any;
   endDate: Date | null;
   onStartDateChange: (date: Date | null) => void;
   onEndDateChange: (date: Date | null) => void;
 };
 
 function DateDrawer({ isOpen, onClose, startDate, endDate, onStartDateChange, onEndDateChange }: DateDrawerProps) {
+  const handleStartDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const date = new Date(e.target.value);
+    onStartDateChange(date);
+  };
+
+  const handleEndDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const date = new Date(e.target.value);
+    onEndDateChange(date);
+  };
+
   return (
     <Drawer open={isOpen} onClose={onClose} direction="right" size={450}>
       <div className="flex flex-col gap-2">
@@ -21,12 +31,11 @@ function DateDrawer({ isOpen, onClose, startDate, endDate, onStartDateChange, on
           Check In
         </Heading>
         <form className="flex flex-col gap-2">
-          <input type="date" value={startDate?.toISOString().slice(0, 10)} onChange={(e) => onStartDateChange(new Date(e.target.value))} />
+          <input type="date" value={startDate?.toISOString().split("T")[0]} onChange={handleStartDateChange} />
           <Heading size={2} color="black">
             Check Out
           </Heading>
-          <input type="date" value={endDate?.toISOString().slice(0, 10)} onChange={(e) => onEndDateChange(new Date(e.target.value))} />
-          {/* button to close drawer */}
+          <input type="date" value={endDate?.toISOString().split("T")[0]} onChange={handleEndDateChange} /> {/* button to close drawer */}
           <Button onClick={onClose} color="charcoal" isFullWidth={true} isActive={true}>
             Select
           </Button>
