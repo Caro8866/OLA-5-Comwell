2169e3bd5a70e4b7db31cc17427207d9d003184a
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-24 21:10:46 +0100

    fix: font wieght on inputSelect

--- frontend/components/formField/InputSelect.tsx
+++ frontend/components/formField/InputSelect.tsx
@@ -16,36 +16,17 @@ function InputSelect({ label, value, onClick }: Props) {
 
   return (
     <div
-      className={`flex max-content flex-col border-2 rounded border-gray-300 px-3 py-4 relative transition hover:border-gray-400 ${
-        isFocused && "border-gray-800 hover:border-gray-800"
-      } `}
+      className={`flex max-content flex-col border-2 rounded border-gray-300 px-3 py-4 relative transition hover:border-gray-400 ${isFocused && "border-gray-800 hover:border-gray-800"} `}
       onClick={() => {
         handleClick();
         onClick();
       }}
     >
-      <label
-        className={`font-sans text-sm absolute bottom-2/4 transition translate-y-[-10%] font-medium `}
-      >
-        {label}
-      </label>
-      <p className={"font-semibold text-lg translate-y-1/4 bg-transparent "}>
-        {value}
-      </p>
-      <div
-        className={`pointer-events-none absolute right-3 bottom-2/4 flex min-content px-2 py-2 translate-y-[50%] justify-center`}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 20"
-          className="w-[16px] rotate-180"
-        >
-          <path
-            stroke="currentColor"
-            stroke-width="1.5"
-            d="M16.666 12.916 10 6.666l-6.667 6.25"
-          ></path>
+      <label className={`font-sans text-sm absolute bottom-2/4 transition translate-y-[-10%] font-light `}>{label}</label>
+      <p className={"text-lg translate-y-1/4 bg-transparent font-light "}>{value}</p>
+      <div className={`pointer-events-none absolute right-3 bottom-2/4 flex min-content px-2 py-2 translate-y-[50%] justify-center`}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" className="w-[16px] rotate-180">
+          <path stroke="currentColor" stroke-width="1.5" d="M16.666 12.916 10 6.666l-6.667 6.25"></path>
         </svg>
       </div>
     </div>
