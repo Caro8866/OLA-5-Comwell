a19e59b465a44a040cd346d20304f3b70006275d
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-24 21:50:51 +0100

    feat: add conference and banquet forms

--- frontend/components/searchWidget/SearchWidget.tsx
+++ frontend/components/searchWidget/SearchWidget.tsx
@@ -66,6 +66,34 @@ function SearchWidget() {
           </Button>
         </form>
       )}
+      {bookingType === "conference" && (
+        <form className="flex flex-col space-y-2 ">
+          <InputSelect label="Amount of participants" onClick={() => {}} value={"8"} />
+          <InputSelect label="Hotel" onClick={() => {}} value={"Choose hotel"} />
+          <InputSelect label="Date" onClick={() => {}} value={"25 Nov - 26 Nov"} />
+          <DualInputSelect label1={"Start"} value1={"08:00"} label2={"End"} value2={"16:00"} onClick={() => {}} />
+          <Button color="charcoal" isFullWidth={true} isActive={false} onClick={() => {}} styles="flex items-center justify-center gap-x-1 font-light">
+            Request
+            <svg xmlns="http://www.w3.org/2000/svg" height="20q" viewBox="0 -960 960 960" width="20q" fill="#fff">
+              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+            </svg>
+          </Button>
+        </form>
+      )}
+      {bookingType === "banquet" && (
+        <form className="flex flex-col space-y-2 ">
+          <InputSelect label="Event type" onClick={() => {}} value={"Choose tyle"} />
+          <InputSelect label="Amount of participants" onClick={() => {}} value={"25"} />
+          <InputSelect label="Hotel" onClick={() => {}} value={"Choose hotel"} />
+          <InputSelect label="Date" onClick={() => {}} value={"25 Nov - 26 Nov"} />
+          <Button color="charcoal" isFullWidth={true} isActive={false} onClick={() => {}} styles="flex items-center justify-center gap-x-1 font-light">
+            Request
+            <svg xmlns="http://www.w3.org/2000/svg" height="20q" viewBox="0 -960 960 960" width="20q" fill="#fff">
+              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+            </svg>
+          </Button>
+        </form>
+      )}
     </div>
   );
 }
