64a05776c3263f76b6bdaf0bd5e07786b1ede71a
George <georgenico9@gmail.com>
2023-11-26 00:19:57 +0100

    feat: validation logic for login

--- frontend/components/header/userForms/SignInForm.tsx
+++ frontend/components/header/userForms/SignInForm.tsx
@@ -2,6 +2,7 @@ import InputField from "@/components/formField/InputField";
 import BodyText from "@/components/text/bodyText/BodyText";
 import { useState, FormEvent } from "react";
 import { SignInValidators } from "./formTypes";
+import InputError from "@/components/formField/InputError";
 
 export default function SignInForm({
   toggleRegisterDrawer,
@@ -12,6 +13,8 @@ export default function SignInForm({
 }) {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [unauthorizedError, setUnauthorizedError] = useState(false);
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
 
   const validators: SignInValidators = {
     loginEmail: {
@@ -28,6 +31,52 @@ export default function SignInForm({
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
     console.log("da");
+    Object.entries(validators).forEach(([key, value]) => {
+      if (!value.validationFunction()) {
+        setValidationErrors((prev) =>
+          Array.from(new Set([...prev, value.fieldName]))
+        );
+      } else {
+        setValidationErrors((prev) =>
+          prev.filter((e) => e !== value.fieldName)
+        );
+      }
+    });
+
+    if (
+      validators.loginEmail.validationFunction() &&
+      validators.loginPassword.validationFunction()
+    ) {
+      fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: loginEmail,
+          password: loginPassword,
+        }),
+      })
+        .then(async (response) => {
+          if (!response.ok) {
+            return response.json().then((errorData) => {
+              // Display an error if the user doesn't exist or the password is wrong
+              setUnauthorizedError(true);
+              throw new Error(`Server error! Message: ${errorData.message}`);
+            });
+          }
+          // Parse the response data as needed
+          return response.json();
+        })
+        .then((data) => {
+          // remove error if the login was successful
+          setUnauthorizedError(false);
+          console.log("Response:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error.message);
+        });
+    }
   }
 
   return (
@@ -38,27 +87,46 @@ export default function SignInForm({
         isLoginVisible ? "" : "hidden"
       }`}
     >
-      <div className={`px-4 pt-6 pb-3 flex flex-col`}>
+      <div className={`px-4 pt-6 pb-3 flex flex-col gap-4`}>
         <InputField
           onChange={(e) => {
             setLoginEmail(e.target.value);
           }}
           value={loginEmail}
-          name="email"
           id="email"
+          name="email"
           label="Email"
-        ></InputField>
+          styles={`w-96`}
+          type="email"
+          errorMessage="Please use a valid email address"
+          validationCondition={() => validators.loginEmail.validationFunction()}
+          validationOnSend={!validationErrors.includes("email")}
+          setValidationErrors={setValidationErrors}
+        />
+
         <InputField
           onChange={(e) => {
             setLoginPassword(e.target.value);
           }}
           value={loginPassword}
-          name="password"
           id="password"
+          name="password"
           label="Password"
           type="password"
-          styles="mt-2"
-        ></InputField>
+          styles={`w-96`}
+          errorMessage="You need to fill in a password"
+          validationCondition={() =>
+            validators.loginPassword.validationFunction()
+          }
+          validationOnSend={!validationErrors.includes("password")}
+          setValidationErrors={setValidationErrors}
+        />
+
+        <InputError
+          message="Something went wrong. Try again"
+          showError={unauthorizedError}
+        />
+
         <BodyText size={1} styles="text-charcoal-60 mt-2 font-regular">
           Forgot your password?
         </BodyText>
