31b8eb4bb5dd75194a10d329436cbd33725065fb
Fryderyk Boncler <f.relcnob@gmail.com>
2023-11-26 14:25:00 +0100

    feat: added addons styling

--- frontend/components/drawers/bookingForm/formSteps/addons/Addons.tsx
+++ frontend/components/drawers/bookingForm/formSteps/addons/Addons.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { BookingContext } from "@/context/BookingContext";
 import Heading from "@/components/text/heading/Heading";
+import BodyText from "@/components/text/bodyText/BodyText";
+import Button from "@/components/button/Button";
 
 type AddonsProps = {
   onNext: () => void;
@@ -9,29 +11,104 @@ type AddonsProps = {
 function Addons({ onNext }: AddonsProps) {
   const { bookingData, setBookingData } = useContext(BookingContext);
 
-  const handleAddonChange = (addon: { name: string; price: number; description?: string; image?: string }, isChecked: boolean) => {
+  const handleAddonChange = (
+    addon: {
+      name: string;
+      price: number;
+      description?: string;
+      image?: string;
+    },
+    isChecked: boolean
+  ) => {
     if (isChecked) {
-      setBookingData({ ...bookingData, selectedAddons: [...bookingData.selectedAddons, addon] });
+      setBookingData({
+        ...bookingData,
+        selectedAddons: [...bookingData.selectedAddons, addon],
+      });
     } else {
-      setBookingData({ ...bookingData, selectedAddons: bookingData.selectedAddons.filter((a) => a.name !== addon.name) });
+      setBookingData({
+        ...bookingData,
+        selectedAddons: bookingData.selectedAddons.filter(
+          (a) => a.name !== addon.name
+        ),
+      });
     }
   };
 
   return (
-    <div>
-      <Heading size={3} color="black" styles="font-light">
+    <div className={`h-full flex flex-col`}>
+      <Heading size={3} color="black" styles="font-light mb-6">
         Select Addons
       </Heading>
-      {bookingData.hotel.addons.map((addon: { name: string; price: number; description?: string; image?: string }) => (
-        <div>
-          <input type="checkbox" onChange={(e) => handleAddonChange(addon, e.target.checked)} />
-          <p>{addon.name}</p>
-          <p>{addon.price}</p>
-          <p>{addon.description}</p>
-        </div>
-      ))}
-
-      <button onClick={onNext}>Next</button>
+      <section className={`grid grid-cols-2 gap-4`}>
+        {bookingData.hotel &&
+          bookingData.hotel.addons.map(
+            (addon: {
+              name: string;
+              price: number;
+              description?: string;
+              image?: string;
+            }) => (
+              <div
+                className={`border rounded-lg flex flex-col p-4 group hover:border-charcoal-80 ${
+                  bookingData.selectedAddons.includes(addon)
+                    ? `border-charcoal-80`
+                    : ""
+                }`}
+                onClick={(e) => {
+                  let isAlreadySelected = false;
+                  if (bookingData.selectedAddons.includes(addon)) {
+                    isAlreadySelected = true;
+                  }
+                  handleAddonChange(addon, !isAlreadySelected);
+                }}
+              >
+                {/* <input
+                  type="checkbox"
+                  onChange={(e) => handleAddonChange(addon, e.target.checked)}
+                /> */}
+                <div className={`flex flex-row justify-between mb-2 gap-4`}>
+                  <div className={`flex flex-row justify-between w-full`}>
+                    <Heading size={5}>{addon.name}</Heading>{" "}
+                    <Heading size={5}>{`${addon.price} kr.`}</Heading>
+                  </div>
+                  <span
+                    className={`min-w-[24px] min-h-[24px] border-box border rounded-full group-hover:border-charcoal-80 flex justify-center items-center p-[1px] ${
+                      bookingData.selectedAddons.includes(addon)
+                        ? `!bg-charcoal-80 border-charcoal-80`
+                        : ""
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 -960 960 960"
+                      width="24"
+                      className={`w-3/4 opacity-0 fill-slate-50 w-[18px] h-[18px] ${
+                        bookingData.selectedAddons.includes(addon)
+                          ? `!opacity-100`
+                          : ""
+                      }`}
+                    >
+                      <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+                    </svg>
+                  </span>
+                </div>
+                <BodyText size={1} styles={`tracking-tighter text-charcoal-80`}>
+                  {addon.description}
+                </BodyText>
+              </div>
+            )
+          )}
+      </section>
+      <div className={`flex grow`}></div>
+      <section
+        className={`flex flex-row self-end w-full mb-0 py-6 border-t justify-end`}
+      >
+        <Button color={"sea"} isActive onClick={onNext} styles="flex">
+          Next
+        </Button>
+      </section>
     </div>
   );
 }
