3292f961bf934f14658da468889f82b4076c6bf5
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-26 13:59:48 +0100

    chore: add search widget to index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -13,6 +13,8 @@ import { Package } from "@/utils/Package.types";
 import Spinner from "@/components/spinner/Spinner";
 import { Offer } from "@/utils/Offer.types";
 import { AuthContextProvider } from "@/context/AuthContext";
+import SearchWidget from "@/components/searchWidget/SearchWidget";
+import { BookingContextProvider } from "@/context/BookingContext";
 
 function index() {
   const [arePkgLoading, setArePkgLoading] = useState(false);
@@ -63,32 +65,15 @@ function index() {
       <AuthContextProvider>
         <Header />
         <main className={`max-w-screen overflow-hidden`}>
-          <section
-            id="hero"
-            className={`w-full h-[calc(100vh-118px)] relative pt-[86px]`}
-          >
-            <div
-              className={`w-full h-full z-20 relative max-w-2xl 2xl:max-w-[1600px] mx-auto`}
-            >
-              <Heading size={1} color="white">
-                TEST
-              </Heading>
-              {/* booking form here */}
+          <section id="hero" className={`w-full h-[calc(100vh-118px)] relative pt-[86px]`}>
+            <div className={`w-full h-full z-20 relative max-w-2xl 2xl:max-w-[1600px] mx-auto`}>
+              <BookingContextProvider>
+                <SearchWidget />
+              </BookingContextProvider>
             </div>
-            <Image
-              src="/img/hero.jpg"
-              alt="placeholder"
-              height={1000}
-              width={1920}
-              className={
-                "w-full brightness-90 contrast-[1.1] h-[calc(100vh-118px)] object-cover absolute top-0 left-0"
-              }
-            />
+            <Image src="/img/hero.jpg" alt="placeholder" height={1000} width={1920} className={"w-full brightness-90 contrast-[1.1] h-[calc(100vh-118px)] object-cover absolute top-0 left-0"} />
           </section>
-          <section
-            id=""
-            className={`max-w-screen-2xl 2xl:max-w-[1600px] grid sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 py-20 px-8 mx-auto`}
-          >
+          <section id="" className={`max-w-screen-2xl 2xl:max-w-[1600px] grid sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 py-20 px-8 mx-auto`}>
             {areOffersLoading && (
               <div className={`col-span-3`}>
                 <Spinner />
@@ -98,24 +83,10 @@ function index() {
               offers.length &&
               offers
                 .slice(-3)
-                .map((offer, index) => (
-                  <InfoCard
-                    key={offer._id}
-                    src={offer.image}
-                    title={offer.name}
-                    description={offer.description}
-                    label={offer.tag}
-                    href={`/${offer.href}`}
-                    styles={`${
-                      index == 0 ? `col-span-2` : `col-span-1`
-                    } md:col-span-1`}
-                  />
-                ))}
+                .map((offer, index) => <InfoCard key={offer._id} src={offer.image} title={offer.name} description={offer.description} label={offer.tag} href={`/${offer.href}`} styles={`${index == 0 ? `col-span-2` : `col-span-1`} md:col-span-1`} />)}
           </section>
           <section className={`py-20  relative`}>
-            <div
-              className={`px-8 mx-auto 2xl:max-w-[1600px] mb-8 w-full flex flex-row justify-between`}
-            >
+            <div className={`px-8 mx-auto 2xl:max-w-[1600px] mb-8 w-full flex flex-row justify-between`}>
               <Heading size={3}>Offers & Experiences</Heading>
               <Button
                 color="outline"
@@ -132,10 +103,7 @@ function index() {
             <div className={`w-screen overflow-visible`}>
               {arePkgLoading && <Spinner />}
               {packages.length > 0 && !arePkgLoading && (
-                <div
-                  ref={sliderRef}
-                  className={`keen-slider !overflow-visible max-w-[1600px] mx-auto px-8`}
-                >
+                <div ref={sliderRef} className={`keen-slider !overflow-visible max-w-[1600px] mx-auto px-8`}>
                   {" "}
                   {packages.map((pkg, index) => {
                     return (
@@ -147,9 +115,7 @@ function index() {
                         description={pkg.description}
                         tag={pkg.type.toUpperCase()}
                         price={pkg.price}
-                        discount={
-                          pkg.discount && pkg.discount > 0 ? pkg.discount : 0
-                        }
+                        discount={pkg.discount && pkg.discount > 0 ? pkg.discount : 0}
                         styles={`keen-slider__slide number-slide${index}`}
                       />
                     );
