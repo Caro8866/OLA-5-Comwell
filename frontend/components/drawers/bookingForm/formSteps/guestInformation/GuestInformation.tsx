6677a0b6007a03689143034c9b59ee419381c1b3
George <georgenico9@gmail.com>
2023-11-26 16:15:34 +0100

    feat: implement logic and validation to guest information

--- frontend/components/drawers/bookingForm/formSteps/guestInformation/GuestInformation.tsx
+++ frontend/components/drawers/bookingForm/formSteps/guestInformation/GuestInformation.tsx
@@ -1,7 +1,8 @@
 import InputField from "@/components/formField/InputField";
 import Heading from "@/components/text/heading/Heading";
 import { BookingContext } from "@/context/BookingContext";
-import { useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
 type GuestInformationProps = {
   onNext: () => void;
@@ -10,12 +11,56 @@ type GuestInformationProps = {
 function GuestInformation({ onNext }: GuestInformationProps) {
   const { bookingData, setBookingData } = useContext(BookingContext);
   const totalPrice = Math.round(bookingData.selectedRoom.price * bookingData.selectedPackage.price + bookingData.selectedAddons.reduce((a, b) => a + b.price, 0));
+  const { authState } = useContext(AuthContext);
+  const isFormSubmitted = useRef(false);
+
+  // first check if the user already completed the booking form ie they came back tp the GuestInformation form
+  // if it's the first time on this form check if they are logged in and prefill the information
+  const [fullName, setFullName] = useState(bookingData.guestInformation.fullName ? bookingData.guestInformation.fullName : authState.userData ? authState.userData.fullName : "");
+  const [email, setEmail] = useState(bookingData.guestInformation.email ? bookingData.guestInformation.email : authState.userData ? authState.userData.email : "");
+  const [phone, setPhone] = useState(bookingData.guestInformation.phone ? bookingData.guestInformation.phone : authState.userData ? authState.userData.phone : "");
+  const [address, setAddress] = useState(bookingData.guestInformation.address ? bookingData.guestInformation.address : "");
+
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+  const validators = {
+    fullName: {
+      fieldName: "fullName",
+      validationFunction: () => fullName.split(/\s+/).length >= 2,
+    },
+
+    email: {
+      fieldName: "email",
+      validationFunction: () => email.includes("@") && email.includes("."),
+    },
+
+    phone: {
+      fieldName: "phone",
+      validationFunction: () => (phone ? Number.isInteger(Number(phone)) : false),
+    },
+
+    address: {
+      fieldName: "address",
+      validationFunction: () => address.trim() !== "",
+    },
+  };
+
+  useEffect(() => {
+    if (validationErrors.length === 0 && isFormSubmitted.current) {
+      setBookingData({ ...bookingData, guestInformation: { ...bookingData.guestInformation, fullName, email, phone, address } });
+      onNext();
+    } else {
+      isFormSubmitted.current = false;
+    }
+  }, [validationErrors]);
+
   return (
     <div>
       <Heading size={2} color="black" styles="font-light">
         Guest Information
       </Heading>
       <form
+        className="flex flex-col gap-4"
         onSubmit={(event) => {
           event.preventDefault();
         }}
@@ -25,19 +70,49 @@ function GuestInformation({ onNext }: GuestInformationProps) {
           name="fullName"
           id="fullName"
           type="text"
-          value={bookingData.guestInformation.fullName}
-          onChange={(e) => setBookingData({ ...bookingData, guestInformation: { ...bookingData.guestInformation, fullName: e.target.value } })}
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          errorMessage="You need to fill in your full name"
+          validationCondition={() => validators.fullName.validationFunction()}
+          validationOnSend={!validationErrors.includes("fullName")}
+          setValidationErrors={setValidationErrors}
         />
 
-        <InputField label="Email" name="email" id="email" type="email" value={bookingData.guestInformation.email} onChange={(e) => setBookingData({ ...bookingData, guestInformation: { ...bookingData.guestInformation, email: e.target.value } })} />
-        <InputField label="Phone" name="phone" id="phone" type="text" value={bookingData.guestInformation.phone} onChange={(e) => setBookingData({ ...bookingData, guestInformation: { ...bookingData.guestInformation, phone: e.target.value } })} />
+        <InputField
+          label="Email"
+          name="email"
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          errorMessage="Invalid email. Please verify your details"
+          validationCondition={() => validators.email.validationFunction()}
+          validationOnSend={!validationErrors.includes("email")}
+          setValidationErrors={setValidationErrors}
+        />
+        <InputField
+          label="Phone"
+          name="phone"
+          id="phone"
+          type="text"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          errorMessage="Invalid phone number. Please verify your details"
+          validationCondition={() => validators.phone.validationFunction()}
+          validationOnSend={!validationErrors.includes("phone")}
+          setValidationErrors={setValidationErrors}
+        />
         <InputField
           label="Address"
           name="address"
           id="address"
           type="text"
-          value={bookingData.guestInformation.address}
-          onChange={(e) => setBookingData({ ...bookingData, guestInformation: { ...bookingData.guestInformation, address: e.target.value } })}
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          errorMessage="Please enter an address"
+          validationCondition={() => validators.address.validationFunction()}
+          validationOnSend={!validationErrors.includes("address")}
+          setValidationErrors={setValidationErrors}
         />
       </form>
       {/* overview section should be a seperate componenet as it is used on many booking form steps*/}
@@ -48,7 +123,21 @@ function GuestInformation({ onNext }: GuestInformationProps) {
       <p>{bookingData.selectedAddons.map((a) => a.name)}</p>
       <p>{bookingData.selectedAddons.map((a) => a.price)}</p>
       <p>{totalPrice}</p> {/* with room price * package price + addons price */}
-      <button onClick={onNext}>Next</button>
+      <button
+        onClick={() => {
+          console.log(validationErrors);
+          isFormSubmitted.current = true;
+          Object.entries(validators).forEach(([key, value]) => {
+            if (!value.validationFunction()) {
+              setValidationErrors((prev) => Array.from(new Set([...prev, value.fieldName])));
+            } else {
+              setValidationErrors((prev) => prev.filter((e) => e !== value.fieldName));
+            }
+          });
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 }
