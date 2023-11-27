8f3e2d42c0b70a7fb202fa9ef5d5407db6a8d4ae
Fryderyk Boncler <f.relcnob@gmail.com>
2023-11-27 09:00:56 +0100

    chore: fixed imports on index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,16 +9,16 @@ import ExperienceCard from "@/components/experienceCard/ExperienceCard";
 import Label from "@/components/label/Label";
 import Button from "@/components/button/Button";
 import Footer from "@/components/footer/Footer";
-import { Package } from "@/utils/Package.types";
+import { HotelPackage } from "@/utils/HotelPackage.types";
 import Spinner from "@/components/spinner/Spinner";
-import { Offer } from "@/utils/Offer.types";
+import { Offer } from "@/utils/offer.types";
 import { AuthContextProvider } from "@/context/AuthContext";
 import SearchWidget from "@/components/searchWidget/SearchWidget";
 import { BookingContextProvider } from "@/context/BookingContext";
 
 function index() {
   const [arePkgLoading, setArePkgLoading] = useState(false);
-  const [packages, setPackages] = useState<Package[]>([]);
+  const [packages, setPackages] = useState<HotelPackage[]>([]);
   const [areOffersLoading, setAreOffersLoading] = useState(false);
   const [offers, setOffers] = useState<Offer[]>([]);
   const [sliderRef, slider] = useKeenSlider(
@@ -41,7 +41,7 @@ function index() {
     setAreOffersLoading(true);
     fetch("http://localhost:5000/packages")
       .then((response) => response.json())
-      .then((data: Package[]) => {
+      .then((data: HotelPackage[]) => {
         setPackages(data);
         setArePkgLoading(false);
       })
@@ -64,17 +64,35 @@ function index() {
     <>
       <AuthContextProvider>
         <Header />
-        <div className={`w-fit h-[calc(100vh-204px)] absolute pt-[86px] pl-[100px] top-[86px] left-0 z-50`}>
+        <div
+          className={`w-fit h-[calc(100vh-204px)] absolute pt-[86px] pl-[100px] top-[86px] left-0 z-50`}
+        >
           <BookingContextProvider>
             <SearchWidget />
           </BookingContextProvider>
         </div>
         <main className={`max-w-screen overflow-hidden`}>
-          <section id="hero" className={`w-full h-[calc(100vh-118px)] relative pt-[86px] z-20`}>
-            <div className={`w-full h-full z-20 relative max-w-2xl 2xl:max-w-[1600px] mx-auto`}></div>
-            <Image src="/img/hero.jpg" alt="placeholder" height={1000} width={1920} className={"w-full brightness-90 contrast-[1.1] h-[calc(100vh-118px)] object-cover absolute top-0 left-0"} />
+          <section
+            id="hero"
+            className={`w-full h-[calc(100vh-118px)] relative pt-[86px] z-20`}
+          >
+            <div
+              className={`w-full h-full z-20 relative max-w-2xl 2xl:max-w-[1600px] mx-auto`}
+            ></div>
+            <Image
+              src="/img/hero.jpg"
+              alt="placeholder"
+              height={1000}
+              width={1920}
+              className={
+                "w-full brightness-90 contrast-[1.1] h-[calc(100vh-118px)] object-cover absolute top-0 left-0"
+              }
+            />
           </section>
-          <section id="" className={`max-w-screen-2xl 2xl:max-w-[1600px] grid sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 py-20 px-8 mx-auto`}>
+          <section
+            id=""
+            className={`max-w-screen-2xl 2xl:max-w-[1600px] grid sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 py-20 px-8 mx-auto`}
+          >
             {areOffersLoading && (
               <div className={`col-span-3`}>
                 <Spinner />
@@ -84,10 +102,24 @@ function index() {
               offers.length &&
               offers
                 .slice(-3)
-                .map((offer, index) => <InfoCard key={offer._id} src={offer.image} title={offer.name} description={offer.description} label={offer.tag} href={`/${offer.href}`} styles={`${index == 0 ? `col-span-2` : `col-span-1`} md:col-span-1`} />)}
+                .map((offer, index) => (
+                  <InfoCard
+                    key={offer._id}
+                    src={offer.image}
+                    title={offer.name}
+                    description={offer.description}
+                    label={offer.tag}
+                    href={`/${offer.href}`}
+                    styles={`${
+                      index == 0 ? `col-span-2` : `col-span-1`
+                    } md:col-span-1`}
+                  />
+                ))}
           </section>
           <section className={`py-20  relative`}>
-            <div className={`px-8 mx-auto 2xl:max-w-[1600px] mb-8 w-full flex flex-row justify-between`}>
+            <div
+              className={`px-8 mx-auto 2xl:max-w-[1600px] mb-8 w-full flex flex-row justify-between`}
+            >
               <Heading size={3}>Offers & Experiences</Heading>
               <Button
                 color="outline"
@@ -104,7 +136,10 @@ function index() {
             <div className={`w-screen overflow-visible`}>
               {arePkgLoading && <Spinner />}
               {packages.length > 0 && !arePkgLoading && (
-                <div ref={sliderRef} className={`keen-slider !overflow-visible max-w-[1600px] mx-auto px-8`}>
+                <div
+                  ref={sliderRef}
+                  className={`keen-slider !overflow-visible max-w-[1600px] mx-auto px-8`}
+                >
                   {" "}
                   {packages.map((pkg, index) => {
                     return (
@@ -116,7 +151,9 @@ function index() {
                         description={pkg.description}
                         tag={pkg.type.toUpperCase()}
                         price={pkg.price}
-                        discount={pkg.discount && pkg.discount > 0 ? pkg.discount : 0}
+                        discount={
+                          pkg.discount && pkg.discount > 0 ? pkg.discount : 0
+                        }
                         styles={`keen-slider__slide number-slide${index}`}
                       />
                     );
