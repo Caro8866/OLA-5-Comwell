d6df5df2179ecf1ed5af2d7e03663f0aeb0152da
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-11-27 10:11:30 +0100

    fix button color

--- frontend/components/searchWidget/SearchWidget.tsx
+++ frontend/components/searchWidget/SearchWidget.tsx
@@ -64,7 +64,7 @@ function SearchWidget({ bookingType, setBookingType, handleHotelDrawerOpen, hand
             )}
             {isBookingCodeInputVisible && <InputField name="bookingCode" id="bookingCode" label="Booking code" onChange={() => alert("Feature not available yet")} value={"Enter booking code"} />}
 
-            <Button color="charcoal" isFullWidth={true} isActive={bookingData.hotel && bookingData.startDate && bookingData.endDate ? true : false} onClick={handleSearch} styles="flex items-center justify-center gap-x-1 font-light">
+            <Button color="sea" isFullWidth={true} isActive={bookingData.hotel && bookingData.startDate && bookingData.endDate ? true : false} onClick={handleSearch} styles="flex items-center justify-center gap-x-1 font-light">
               Search
               <svg xmlns="http://www.w3.org/2000/svg" height="20q" viewBox="0 -960 960 960" width="20q" fill="#fff">
                 <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
