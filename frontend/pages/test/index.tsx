993c482357acc8150f9121f1a1198941960365ac
Caroline Thostrup <thostrupcaroline@gmail.com>
2023-12-04 10:45:52 +0100

    fix: test page

--- frontend/pages/test/index.tsx
+++ frontend/pages/test/index.tsx
@@ -2,25 +2,18 @@ import "@/app/globals.css";
 // import BookingForm from "@/components/bookingForm/BookingForm";
 import ExperienceCard from "@/components/experienceCard/ExperienceCard";
 import InputSelect from "@/components/formField/InputSelect";
-import Header from "@/components/header/Header";
-import SearchWidget from "@/components/searchWidget/SearchWidget";
-import { BookingContextProvider } from "@/context/BookingContext";
-import { log } from "console";
 import React from "react";
-import { useState } from "react";
-import { AuthContextProvider } from "@/context/AuthContext";
 
 function Test() {
-  const [myValue, setMyValue] = useState("Test");
-
   return (
-    <AuthContextProvider>
+    <h1>Test page</h1>
+    /*   <AuthContextProvider>
       <div className={`h-full w-full bg-slate-400`}>
         <BookingContextProvider>
           <SearchWidget />
         </BookingContextProvider>
       </div>
-    </AuthContextProvider>
+    </AuthContextProvider> */
   );
 }
 
