ae04384fcd65c8192d1615150947fa7430ec2972
Fryderyk Boncler <f.relcnob@gmail.com>
2023-12-17 11:17:36 +0100

    fix: resolved docker issues

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({origin: "*"});
   app.use(cookieParser());
   await app.listen(5000);
   console.log('Server is now live.');
