9afe6f6aa5812136d228a9173f36dc87212804d4
Fryderyk Boncler <f.relcnob@gmail.com>
2023-12-24 11:38:01 +0100

    fix: fixed ID

--- backend/src/hotels/hotels.controller.ts
+++ backend/src/hotels/hotels.controller.ts
@@ -31,17 +31,17 @@ export class HotelsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.hotelsService.findOne(+id);
+    return this.hotelsService.findOne(id);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateHotelDto: UpdateHotelDto) {
-    return this.hotelsService.update(+id, updateHotelDto);
+    return this.hotelsService.update(id, updateHotelDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.hotelsService.remove(+id);
+    return this.hotelsService.remove(id);
   }
 
   @Post(':id/packages')
