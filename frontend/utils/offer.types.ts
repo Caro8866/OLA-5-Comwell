45392ea0d7b02f3d41c2b15748c37701f8d8ec64
Fryderyk Boncler <f.relcnob@gmail.com>
2023-12-28 15:44:51 +0100

    feat: added add offer

--- frontend/utils/offer.types.ts
+++ frontend/utils/offer.types.ts
@@ -6,3 +6,11 @@ export type Offer = {
   image: string;
   href: string;
 };
+
+export type OfferForm = {
+  name: string;
+  description: string;
+  tag: string;
+  image: string;
+  href: string;
+};
